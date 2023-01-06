b25722a0375e02341333b2b844a9d09e70aee11f
thepromaper <davidcampeon2007@gmail.com>
2023-01-06 09:45:38 -0500

    icons

--- forge.config.js
+++ forge.config.js
@@ -1,25 +1,31 @@
-module.exports={
-    "packagerConfig": {},
-    "makers": [
-      {
-        "name": "@electron-forge/maker-squirrel",
-        "config": {
-          "name": "electron_quick_start"
-        }
+module.exports = {
+  packagerConfig: {
+    icon: '/public/icons/icon',
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        name: 'electron_quick_start',
       },
-      {
-        "name": "@electron-forge/maker-zip",
-        "platforms": [
-          "darwin"
-        ]
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: [
+        'darwin',
+      ],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          icon: './public/icons/icon.png',
+        },
       },
-      {
-        "name": "@electron-forge/maker-deb",
-        "config": {}
-      },
-      {
-        "name": "@electron-forge/maker-rpm",
-        "config": {}
-      }
-    ]
-}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
--- public/main.cjs
+++ public/main.cjs
@@ -27,6 +27,7 @@ function CreateWindow() {
   window = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'icons', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -58,7 +59,7 @@ app.on('window-all-closed', () => {
   window = undefined;
   new Notification({ title: 'smart notes is running', body: 'smart notes will keep checking for reminders' }).show();
   if (tray === undefined) {
-    tray = Tray(path.join(__dirname, 'OnTray.png'));
+    tray = Tray(path.join(__dirname, 'icons', 'on_tray.png'));
     const contextMenu = Menu.buildFromTemplate([
       { label: 'Open in desktop', click: () => { if (window === undefined) { CreateWindow(); } else { window.moveTop(); } } },
       { label: 'close smart notes', click: CloseApp },
