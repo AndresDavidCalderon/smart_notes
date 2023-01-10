27126191cd618f655f92946b0ad28076c06779da
thepromaper <davidcampeon2007@gmail.com>
2023-01-09 22:26:49 -0500

    text selection

--- src/add_note/Text/Editor/MarkDown.jsx
+++ src/add_note/Text/Editor/MarkDown.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -18,6 +18,9 @@ function addChildrenToTag(tag, children, index) {
     }
     case '[i]': {
       return <i key={index}>{children}</i>;
+    }
+    case 'none': {
+      return <span key={index}>{children}</span>;
     }
     default: {
       return 'invalid tag';
@@ -39,11 +42,26 @@ function findNearestTag(string, startingIndex) {
   return nearest;
 }
 
+function getIndexAsRendered(text, index) {
+  let currentTagIndex = 0;
+  let newIndex = index;
+  while (currentTagIndex < index) {
+    const tag = findNearestTag(text, currentTagIndex);
+    if (tag.index < index) {
+      currentTagIndex += tag.index + tag.tag.length;
+      newIndex -= tag.tag.length;
+    } else {
+      break;
+    }
+  }
+  return newIndex;
+}
+
 function getChildren(string, index, endTag) {
   let nearestTag;
   const children = [];
   let length = index;
-  while (true) {
+  while (length < string.length - 1 && (nearestTag === undefined || nearestTag.index !== -1)) {
     nearestTag = findNearestTag(string, length);
     // add text before next tag
     children.push(nearestTag.index === -1
@@ -65,27 +83,88 @@ function getChildren(string, index, endTag) {
   }
   return { children, endTag: nearestTag, length };
 }
+
 function bbcodeToHtml(bbcodeString) {
   let index = 0;
   const children = [];
   while (index < bbcodeString.length - 1) {
     const tag = findNearestTag(bbcodeString, index);
     if (tag.index === -1) {
-      children.push(bbcodeString.substring(index));
+      children.push(bbcodeString.substring(index, tag.index));
       break;
     } else {
       children.push(bbcodeString.substring(index, tag.index));
-      const newBlock = getChildren(bbcodeString, tag.index + tag.tag.length, getEnd(tag.tag));
+      const newBlock = getChildren(
+        bbcodeString,
+        tag.index + tag.tag.length,
+        getEnd(tag.tag),
+      );
       children.push(addChildrenToTag(tag.tag, newBlock.children, tag.index));
       index = newBlock.length;
     }
   }
   return children;
 }
+function charsInNode(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    return node.textContent.length;
+  }
+  return Array.from(node.childNodes).reduce((charachters, currentNode) => {
+    let newCharCount = charachters;
+    switch (node.nodeType) {
+      case Node.ELEMENT_NODE: {
+        newCharCount += charsInNode(currentNode);
+        break;
+      }
+      case Node.TEXT_NODE: {
+        newCharCount += node.textContent.length;
+      }
+      // no default
+    }
+    return newCharCount;
+  }, 0);
+}
+
+// Finds the number of charachters before a certain node in the scope of the limiter element.
+function getCharsUntilNode(node, limiter) {
+  if (limiter === node) {
+    return 0;
+  }
+  return Array.from(node.parentElement.childNodes).reduce((charachters, currentNode, index) => {
+    if (index >= Array.from(node.parentElement.childNodes).indexOf(node)) { return charachters; }
+    return charachters + charsInNode(currentNode);
+  }, 0) + getCharsUntilNode(node.parentElement, limiter);
+}
+function getSelectionIndex(textArea) {
+  const globalSelection = window.getSelection();
+  if ((!textArea.contains(globalSelection.anchorNode))
+  || (!textArea.contains(globalSelection.focusNode))) {
+    return null;
+  }
+  const selection = {
+    start: getCharsUntilNode(globalSelection.anchorNode, textArea) + globalSelection.anchorOffset,
+
+    end: getCharsUntilNode(globalSelection.focusNode, textArea) + globalSelection.focusOffset,
+  };
+  return selection;
+}
 
 function MarkDownEditor({ note, noteChanger }) {
+  const textArea = useRef();
+
+  const [selection, setSelection] = useState(0);
+  const handleSelection = () => {
+    if (getSelectionIndex(textArea.current) !== null) {
+      setSelection(getSelectionIndex(textArea.current));
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('selectionchange', handleSelection);
+    return () => { document.removeEventListener('selectionchange', handleSelection); };
+  });
+
   return (
-    <div contentEditable suppressContentEditableWarning id="content">
+    <div ref={textArea} contentEditable suppressContentEditableWarning id="content">
       {bbcodeToHtml(note.text)}
     </div>
   );
