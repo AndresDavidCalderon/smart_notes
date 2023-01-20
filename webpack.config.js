c09f102a01bf64edfb7d4ec954582f98d1f2ce89
thepromaper <davidcampeon2007@gmail.com>
2023-01-20 14:25:46 -0500

    CSP added correctly in html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const devMode = process.env.NODE_ENV !== 'production';
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const csp = {
+  development: "default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' wss://*.preview.app.github.dev:8080/ws; img-src 'self' data:; script-src 'self' 'unsafe-inline';",
+  production: "default-src 'self'; img-src 'self' data:;",
+};
 
 module.exports = {
   entry: './src/index.jsx',
@@ -36,7 +43,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.ejs',
       templateParameters: {
-        CSP: devMode ? "default-src 'self'; style-src 'self' 'unsafe-inline'; connect-src 'self' wss://*.preview.app.github.dev:8080/ws; img-src 'self' data:;" : "default-src 'self' img-src 'self' data:;",
+        CSP: csp[devMode ? 'development' : 'production'],
       },
     }),
   ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
@@ -55,5 +62,9 @@ module.exports = {
         warnings: false,
       },
     },
+    headers: [{
+      key: 'Content-Security-Policy',
+      value: csp.development,
+    }],
   },
 };
