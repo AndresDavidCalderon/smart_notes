e5f1b225ad3ad70f1acaff5139323572d938fcab
thepromaper <davidcampeon2007@gmail.com>
2023-03-25 12:22:03 -0500

    move devtools path to environment variable

--- android/app/src/main/java/io/voidgames/smartnotes/MainActivity.java
+++ android/app/src/main/java/io/voidgames/smartnotes/MainActivity.java
@@ -2,4 +2,10 @@ package io.voidgames.smartnotes;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(EchoPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
