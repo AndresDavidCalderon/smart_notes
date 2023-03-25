61f568ef0da70549fc65283f1809eb9c29601c50
thepromaper <davidcampeon2007@gmail.com>
2023-03-25 12:52:27 -0500

    max reminders

--- public/reminders.cjs
+++ public/reminders.cjs
@@ -1,8 +1,10 @@
 const { ipcMain, Notification } = require('electron');
 const { removeAttachments, removeBBCode } = require('./noteFormatting');
 
-// As key it has note ID's, and as values timer ID's
-const reminders = {};
+// As key it has note ID's,
+// and as values an object with reminder IDs as key and
+// reminder objects containing the amount of reminders left and last timer ID as value.
+const notes = {};
 
 function getNextReminder(reminder) {
   const currentDate = new Date();
@@ -19,12 +21,13 @@ function getNextReminder(reminder) {
 }
 
 function remind(reminder, note) {
-  // This newText has attachments and BBCode removed
-
   new Notification({ title: 'your reminder!', body: removeBBCode(removeAttachments(note.text)) }).show();
-  // because they use each other
-  // eslint-disable-next-line no-use-before-define
-  planReminder(reminder, note);
+  if (reminder.max_reminders === -1 || reminder.max_reminders > 0) {
+    // because they use each other
+    // eslint-disable-next-line no-use-before-define
+    notes[note.id][reminder.id].timer_id = planReminder(reminder, note);
+    notes[note.id][reminder.id].max_reminders -= 1;
+  }
 }
 
 function planReminder(reminder, note) {
@@ -32,28 +35,28 @@ function planReminder(reminder, note) {
     () => { remind(reminder, note); },
     getNextReminder(reminder).getTime() - (new Date()).getTime(),
   );
-  reminders[note.id].push(timer);
+  return timer;
 }
 
 function addNote(note) {
-  reminders[note.id] = [];
+  notes[note.id] = [];
   note.reminders.forEach((reminder) => {
-    planReminder(reminder, note);
+    notes[note.id][reminder.id] = { ...reminder, timer_id: planReminder(reminder, note) };
   });
 }
 
 function CancelRemindersOfNote(id) {
-  reminders[id].forEach((timeOutID) => {
-    clearTimeout(timeOutID);
+  Object.keys(notes[id]).forEach((reminderID) => {
+    clearTimeout(notes[id][reminderID].timer_id);
   });
 }
 
 // replaces all current reminders for a brand new list
-ipcMain.on('set_notes', (event, notes) => {
-  Object.keys(reminders).forEach((noteID) => {
+ipcMain.on('set_notes', (event, newNotes) => {
+  Object.keys(notes).forEach((noteID) => {
     CancelRemindersOfNote(noteID);
   });
-  notes.forEach((note) => {
+  newNotes.forEach((note) => {
     addNote(note);
   });
 });
@@ -64,5 +67,5 @@ ipcMain.on('add_note', (event, note) => {
 
 ipcMain.on('delete_note', (event, noteID) => {
   CancelRemindersOfNote(noteID);
-  delete reminders[noteID];
+  delete notes[noteID];
 });
